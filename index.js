6f67732f99f9612f836b157981f2fb6bdf0357e2
Joseph Daniels <jdanie37@jhmi.edu>
2015-01-17 14:50:02 -0500

    Initial commit and initial stab at implememntation

---
+++ index.js
@@ -0,0 +1,54 @@
+/* jshint node: true */
+'use strict';
+
+var merge = require('lodash-node/compat/objects/merge');
+var deploymentURLConfigDefaults = {
+  developmentURLPrefix: '',
+  testURLPrefix: '',
+  productionURLPrefix: ''
+};
+
+
+function contentString(jsPrefix, cssPrefix) {
+  return "<link rel='stylesheet' href='" + jsPrefix + "assets/vendor.css'>" +
+    "<link rel='stylesheet' href='" + jsPrefix + "assets/dummy.css'>" +
+    "<script src='" + jsPrefix + "assets/vendor.js'></script>" +
+    "<script src='" + jsPrefix + "assets/dummy.js'></script>";
+
+
+}
+module.exports = {
+  name: 'ember-cli-deployment-url',
+
+
+  contentFor: function (type, config) {
+
+    if (type === 'head') {
+      var deploymentURLConfig = merge({},
+        deploymentURLConfigDefaults,
+        config.deploymentURLConfig);
+      var env = config.environment;
+      var includeStatements;
+      switch (env) {
+        case "development":
+          includeStatements = contentString(deploymentURLConfig.developmentURLPrefix, deploymentURLConfig.developmentURLPrefix);
+
+          break;
+        case "test":
+          includeStatements = contentString(deploymentURLConfig.testURLPrefix, deploymentURLConfig.testURLPrefix);
+
+          break;
+        case "production":
+          includeStatements = contentString(deploymentURLConfig.productionURLPrefix, deploymentURLConfig.productionURLPrefix);
+
+          break;
+        default:
+          break;
+      }
+
+      return contentString;
+
+
+    }
+  }
+};
